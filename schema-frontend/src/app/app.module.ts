de5ee4b0391e7d359adc5ba4bf18dd8a04e57680
amijangid1720 <ajangid@argusoft.com>
2023-12-15 18:27:21 +0530

    Frontend UI

--- schema-frontend/src/app/app.module.ts
+++ schema-frontend/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    FormsModule,
+    InputNumberModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
