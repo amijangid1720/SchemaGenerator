c25a20ad8667a0da7c7408b6f01e01f389c0e9e8
manisha-kumari19 <mkumari@argusoft.com>
2023-12-15 12:25:54 +0530

    initial setup for backend

---
+++ schema-backend/src/main/java/com/schemagenerator/SchemaGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.schemagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaGeneratorApplication.class, args);
+	}
+
+}
