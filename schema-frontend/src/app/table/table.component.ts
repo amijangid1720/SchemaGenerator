4edb82ab639c961b62617347a235432375cca847
manisha-kumari19 <mkumari@argusoft.com>
2023-12-20 19:00:47 +0530

    ui modified

--- schema-frontend/src/app/table/table.component.ts
+++ schema-frontend/src/app/table/table.component.ts
@@ -10,44 +10,91 @@ import { switchMap } from 'rxjs';
 })
 export class TableComponent {
   // tablename:String='';
-  column:any[]=[];
+//   column:any[]=[];
+//   cnames: string[] = [];
+
+//   constructor(
+//     private schemaGenerator: SchemaGeneratorService,
+//     private route: ActivatedRoute
+//   ){}
+
+//   ngOnInit() {
+//     // Subscribe to route parameter changes
+//     this.route.paramMap
+//       .pipe(
+//         switchMap(params => {
+//           const tableName = params.get('tableName');
+//          // Update the tablename property
+//           return this.schemaGenerator.fetchSchema(tableName);
+//         })
+//       )
+//       .subscribe({
+//         next: (response: any) => {
+//           console.log(response);
+
+//           this.column = response.columns;
+//           console.log(this.column);
+
+//           this.cnames = this.column.map((column: any) => column.name);
+//           const columnDataTypes = this.column.map(
+//             (column: any) => column.dataType
+//           );
+//           const primaryKey = this.column.map((column: any) => column.primary);
+
+//           console.log('column names', this.cnames);
+//           console.log('column datatypes', columnDataTypes);
+//           console.log(primaryKey);
+//         },
+//         error: (err) => {
+//           console.log('Error', err);
+//         }
+//       });
+//   }
+
+column: any[] = [];
   cnames: string[] = [];
+  columnData: any[] = []; // Initialize as an empty array
 
   constructor(
     private schemaGenerator: SchemaGeneratorService,
     private route: ActivatedRoute
-  ){}
+  ) {}
 
-  ngOnInit() {
-    // Subscribe to route parameter changes
-    this.route.paramMap
-      .pipe(
-        switchMap(params => {
-          const tableName = params.get('tableName');
-         // Update the tablename property
-          return this.schemaGenerator.fetchSchema(tableName);
-        })
-      )
-      .subscribe({
-        next: (response: any) => {
-          console.log(response);
-
-          this.column = response.columns;
-          console.log(this.column);
-
-          this.cnames = this.column.map((column: any) => column.name);
-          const columnDataTypes = this.column.map(
-            (column: any) => column.dataType
-          );
-          const primaryKey = this.column.map((column: any) => column.primary);
-
-          console.log('column names', this.cnames);
-          console.log('column datatypes', columnDataTypes);
-          console.log(primaryKey);
-        },
-        error: (err) => {
-          console.log('Error', err);
-        }
-      });
-  }
-}
+      // Subscribe to route parameter changes
+      ngOnInit() {
+        // Subscribe to route parameter changes
+        this.route.paramMap
+          .pipe(
+            switchMap(params => {
+              const tableName = params.get('tableName');
+              // Update the tablename property
+              return this.schemaGenerator.fetchSchema(tableName);
+            })
+          )
+          .subscribe({
+            next: (response: any) => {
+              console.log(response);
+      
+              this.column = response.columns;
+              console.log(this.column);
+      
+              this.cnames = this.column.map((column: any) => column.name);
+              // Initialize columnData with a single empty object
+              this.columnData = [{}];
+      
+              const columnDataTypes = this.column.map(
+                (column: any) => column.dataType
+              );
+              const primaryKey = this.column.map((column: any) => column.primary);
+      
+              console.log('column names', this.cnames);
+              console.log('column datatypes', columnDataTypes);
+              console.log(primaryKey);
+            },
+            error: (err) => {
+              console.log('Error', err);
+            }
+          });
+      }
+      
+    }
