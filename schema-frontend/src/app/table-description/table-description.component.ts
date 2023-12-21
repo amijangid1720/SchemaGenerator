d253736b46636280b1a71779c871642d520305ce
manisha-kumari19 <mkumari@argusoft.com>
2023-12-21 18:04:42 +0530

    update schema

--- schema-frontend/src/app/table-description/table-description.component.ts
+++ schema-frontend/src/app/table-description/table-description.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { SchemaGeneratorService } from '../Services/schema-generator.service';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs';
+import { Column } from '../Models/Column';
 
 @Component({
   selector: 'app-table-description',
@@ -11,9 +12,16 @@ import { switchMap } from 'rxjs';
 export class TableDescriptionComponent {
   tablename:String='student11';
   column:any[]=[];
-  // cnames: string[] = [];
-  // cDataTypes:string[] = [];
-  // primaryKey:Boolean[] = [];
+  primaryKeyVisibility: boolean = true;
+  selectedPrimaryIndex: number | null = null;
+
+  // columns = [
+  //   { name: 'column.name', dataType: 'Type1', primary: false, editing: false },
+  //   // { name: 'column.name', dataType: 'Type2', primary: true, editing: false },
+  //   // Add more columns as needed
+  // ];
+  selectedDataType: string = '';
+  
 
   constructor(
     private schemaGenerator: SchemaGeneratorService,
@@ -38,5 +46,51 @@ export class TableDescriptionComponent {
     })
   }
 
+  // editColumn(column: any): void {
+  //   // Set the editing state to true
+  //   column.editing = true;
+  // }
+  checkVisibility(index: number) {
+    this.selectedPrimaryIndex = index;
+    this.column.forEach((column, i) => {
+      column.primary = i === index;
+    });
+  }
+
+  uneditable(event: Event, index: number) {
+    event.preventDefault();
+    console.log('Primary key clicked:', index);
+    // Your existing logic for updating primary keys can go here
+  }
+  
+
+  saveColumn(column: any): void {
+    // Save the changes and set the editing state to false
+    
+    this.column.forEach((col) => (col.editing = false));
+    column.editing = false;
+  }
+
+  cancelEdit(column: any): void {
+    // Cancel the edit and set the editing state to false
+    this.column.forEach((col) => (col.editing = false));
+    column.editing = false;
+  }
+
+OnSelectedDataType(event:any, columnIndex:number, column:Column){
+  console.log(event);
+  console.log(columnIndex);
+  console.log(column);
+  column.dataType=event.target.name;
+  console.log(column.dataType);
+  
+}
+
+toggleEditMode(): void {
+  this.column.forEach((col) => (col.editing = true));
+}
+hasPrimaryKey(column: any): boolean {
+  return this.column.some((col) => col.primary && col !== column);
+}
 }
 
